4b649038300d143b4eb0b067308c0eb398210e1a
Jacob <jacobsolano1988@gmail.com>
2023-03-06 23:13:20 -0600

    Adding permanent photos

--- src/Site Pages/BookPage.js
+++ src/Site Pages/BookPage.js
@@ -11,11 +11,14 @@ const BookPage = () => {
       <p>A valid credit card is required to be kept on file for all appointments.</p>
       <p>There is a 3% charge on all credit card transactions. </p>
       <p>Appointment reminders will be sent 72 and 48 hours ahead of time via text.</p>
+      <p>To reschedule an appointment or book an appointment after 4pm please <Link to="/contact">Contact Me.</Link></p>
+        <div className="book-online-link">
+        <Link to="/contact"><p className="book-online-text">Contact Me</p></Link> </div>
       <div className="book-online-link">
       <a href="https://square.site/book/VZV0FJCBC1YS4/hair-by-katelyn-birky-valparaiso-in" target="_blank" rel="noopener noreferrer">
       <p className="book-online-text">Book Online</p></a></div>
       <div className="cancel-policy">
-      <p>Please View My</p>
+      <p>View My</p>
       <div class="link-text">
       <Link to="/cancellation">
         
--- src/Site Pages/HomePage.js
+++ src/Site Pages/HomePage.js
@@ -33,7 +33,7 @@ function HomePage() {
         >
           <SwiperSlide>
             <div class="image-container">
-              <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t1.6435-9/47792670_2239133116412139_1696221608986804224_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=174925&_nc_ohc=-KxrqqMXh3gAX8o4De6&_nc_ht=scontent-ord5-2.xx&oh=00_AfB6osYThlpbCEswfA6nFBIJqDbHKcnrrCfK7j5vubRruw&oe=640B9390" alt="Slide 1" />
+              <img src="https://i.postimg.cc/DzbHQnN7/main.jpg" alt="Slide 1" />
               <div class="text-overlay">
                 <h1 className="main-overlay-text">Katelyn Birky</h1>
                 <p className="main-overlay-text-2">Hairstylist</p>
@@ -42,7 +42,7 @@ function HomePage() {
           </SwiperSlide>
           <SwiperSlide>
             <div class="image-container">
-              <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/327462593_878067920134276_7845143632512178910_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=OkRSLY3cgdYAX8wSaeA&_nc_ht=scontent-ord5-2.xx&oh=00_AfDrmOdDrnkdSgWrAJPH0Rqj96sLlgl1rYaMxcD2RwntJw&oe=64027A34" alt="Slide 2" />
+              <img src="https://i.postimg.cc/dV3z18mk/Color.jpg" alt="Slide 2" />
               <div class="text-overlay">
                 <h1 className="main-overlay-text">Discover Your Perfect Color</h1>
               </div>
@@ -50,7 +50,7 @@ function HomePage() {
           </SwiperSlide>
           <SwiperSlide>
             <div class="image-container">
-              <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/292675124_451200183679957_5757115862384107057_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lGfbJFZEXM0AX-WlKUM&tn=qZC8PrD-8OvLrnOu&_nc_ht=scontent-ord5-2.xx&oh=00_AfB-N6cTbvgf8zOEgchWSPdhPUJieu44T_j6xN25L57uZg&oe=6401E5EE" alt="Slide 3" />
+              <img src="https://i.postimg.cc/nhWNQC0d/Wedding.jpg" alt="Slide 3" />
               <div class="text-overlay">
                 <h1 className="main-overlay-text">Wedding Dreams Come True</h1>
               </div>
@@ -61,7 +61,7 @@ function HomePage() {
         <div className="static-images">
           <Link to="/services">
             <div class="image-container-static">
-            <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/312951748_535731411893500_7092277791842944265_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=730e14&_nc_ohc=iPoz_NjgtpkAX83GK0H&_nc_ht=scontent-ord5-2.xx&oh=00_AfDIiFSDdCOY1JsVxNXJ798_CiYuaJKV3I7LEt2oxBcajw&oe=6400FDB4" alt="1" />
+            <img src="https://i.postimg.cc/NMMC0Yw6/Pricing-Update.jpg" alt="1" />
               <div class="text-overlay-static">
                 {/* <p className="main-overlay-text-static">Book Now!</p> */}
           </div></div></Link>
@@ -69,7 +69,7 @@ function HomePage() {
 
           <Link to="/book">
             <div class="image-container-static">
-            <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t1.6435-9/165525082_2939725103019600_6506360652530317141_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=a26aad&_nc_ohc=YdPDCWAzjtIAX94Dfa_&_nc_ht=scontent-ord5-2.xx&oh=00_AfDqLOe5D7CFnrCQyc6-QpOxxfDw3vFhNiWMU4sGrBpFPA&oe=640E9F49" alt="2" />
+            <img src="https://i.postimg.cc/63mj2S9D/Booking.jpg" alt="2" />
               <div class="text-overlay-static">
                 {/* <p className="main-overlay-text-static">View Policy</p> */}
           </div>
@@ -79,7 +79,7 @@ function HomePage() {
 
           <Link to="/store">
             <div class="image-container-static">
-            <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t1.6435-9/90905765_2620125341646246_638114677125021696_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=9267fe&_nc_ohc=fEUlK7mSlBIAX-0NY0c&_nc_ht=scontent-ord5-2.xx&oh=00_AfCF5a9yb9nX98aJYJuGhL-Tx-RQ344PS-_cMzVSbhO7eg&oe=640E9278" alt="3" />
+            <img src="https://i.postimg.cc/Vv07jRv5/Online-Store.jpg" alt="3" />
             <div class="text-overlay-static">
               {/* <p className="main-overlay-text-static"></p> */}
             </div>
@@ -89,7 +89,7 @@ function HomePage() {
 
           <Link to="/cancellation">
             <div class="image-container-static">
-            <img src="https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/277728832_3220118411646933_2693453111627552523_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=a26aad&_nc_ohc=XmDJkpPY2xgAX89pxQC&tn=qZC8PrD-8OvLrnOu&_nc_ht=scontent-ord5-2.xx&oh=00_AfCsc4B5mM4W4JNDOiu8XZfyr1eqpDyo_Z9wRHrzYmMt8A&oe=64029B12" alt="4" />
+            <img src="https://i.postimg.cc/1zMYPXbR/Cancellation-Policy.jpg" alt="4" />
             <div class="text-overlay-static">
             </div>
           </div>
